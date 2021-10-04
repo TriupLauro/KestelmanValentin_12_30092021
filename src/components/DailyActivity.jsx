73879c7dc11cc5a380771a2af9cf6fc809bdf4a6
Valentin Kestelman <valaourok@gmail.com>
2021-10-04 17:31:16 +0200

    Fix scale kilogram and calories

--- src/components/DailyActivity.jsx
+++ src/components/DailyActivity.jsx
@@ -14,9 +14,11 @@ class DailyActivity extends Component {
                     margin={{top: 100}}
                 >
                     <XAxis dataKey="day" tickLine={false}/>
-                    <YAxis orientation="right" tickLine={false} axisLine={false}/>
-                    <Bar dataKey="kilogram" fill="#2B2D30" barSize={7} radius={[3,3,0,0]} />
-                    <Bar dataKey="calories" fill="#E60000" barSize={7} radius={[3,3,0,0]} maxBarSize={500}/>
+                    <YAxis yAxisId="kilogram" orientation="right" tickLine={false} axisLine={false} dataKey="kilogram"
+                           domain={['dataMin - 1', 'dataMax']} tickCount={3}/>
+                    <YAxis yAxisId="calories" orientation="right" tickLine={false} axisLine={false} dataKey="calories" hide={true} />
+                    <Bar dataKey="kilogram" fill="#2B2D30" barSize={7} radius={[3,3,0,0]} yAxisId="kilogram" />
+                    <Bar dataKey="calories" fill="#E60000" barSize={7} radius={[3,3,0,0]} yAxisId="calories" />
                     <Legend iconType="circle" align="right" verticalAlign="top" iconSize={8} wrapperStyle={{top:24}}
                             formatter={(value) => {
                                 return <span className="ml-2.5 mr-8 text-subdued text-bar-legend">{BarChartLegend[value]}</span>
