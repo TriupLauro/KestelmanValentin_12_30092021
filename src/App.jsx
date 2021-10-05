69f2c43dca8f83900ff646aca83e62d21a250e63
Valentin Kestelman <valaourok@gmail.com>
2021-10-05 11:07:10 +0200

    Prop types for the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Component} from "react";
 import TopNav from "./components/TopNav";
 import VerticalBar from "./components/VerticalBar";
 import Dashboard from "./components/Dashboard";
+import PropTypes from "prop-types";
 
 class App extends Component {
     render() {
@@ -22,6 +23,60 @@ class App extends Component {
             </>
         )
     }
+}
+
+const scoreBetweenZeroAndOne = (props, propName, componentName) => {
+    if (props[propName]) {
+        let value = props[propName]
+        if (typeof value !== 'number')  return new Error(`${propName} in ${componentName} is not a number`)
+        if (value < 0 || value > 1) return new Error(`${propName} in ${componentName} should have a value between 0 and 1 included`)
+    } else {
+        return new Error(`${propName} in ${componentName} is required`)
+    }
+}
+
+App.propTypes = {
+    mainData : PropTypes.shape({
+        id : PropTypes.number.isRequired,
+        userInfos : PropTypes.shape({
+            firstName : PropTypes.string.isRequired,
+            lastName : PropTypes.string,
+            age : PropTypes.number
+        }),
+        keyData : PropTypes.shape({
+            calorieCount: PropTypes.number.isRequired,
+            proteinCount: PropTypes.number.isRequired,
+            carbohydrateCount: PropTypes.number.isRequired,
+            lipidCount: PropTypes.number.isRequired
+        })
+    }),
+
+    activityData : PropTypes.shape({
+        userId : PropTypes.number.isRequired,
+        sessions : PropTypes.arrayOf(PropTypes.shape({
+            day : PropTypes.string,
+            kilogram : PropTypes.number,
+            calories : PropTypes.number
+        }))
+    }),
+
+    averageSessionsData : PropTypes.shape({
+        userId : PropTypes.number.isRequired,
+        sessions : PropTypes.arrayOf(PropTypes.shape({
+            day : PropTypes.number,
+            sessionLength : PropTypes.number
+        }))
+    }),
+
+    activityTypeData : PropTypes.shape({
+        userId : PropTypes.number.isRequired,
+        data : PropTypes.arrayOf(PropTypes.shape({
+            value : PropTypes.number,
+            kind : PropTypes.number
+        }))
+    }),
+
+    scoreData : scoreBetweenZeroAndOne
 }
 
 export default App
