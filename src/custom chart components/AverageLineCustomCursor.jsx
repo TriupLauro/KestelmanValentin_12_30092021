a06d6c4c20e2b20595f3295dacbf8aa8b220a862
Valentin Kestelman <valaourok@gmail.com>
2021-10-14 13:58:36 +0200

    Tweaked the line chart custom cursor

--- src/custom chart components/AverageLineCustomCursor.jsx
+++ src/custom chart components/AverageLineCustomCursor.jsx
@@ -9,10 +9,14 @@ import {Rectangle} from "recharts";
 class CustomCursor extends Component {
     render() {
         const startX = this.props.points[0]?.x
-        const restWidth = 500 - startX
+        /**
+         * Remove the top bottom left and right positioning to cover the whole chart
+         */
+        const height = this.props.height + this.props.top + this.props.bottom
+        const restWidth = this.props.width - startX + this.props.left + this.props.right
         return(
             <svg>
-                <Rectangle x={startX} y={0} height={500} width={restWidth} fill="black" opacity={0.2} radius={[0,6,6,0]}/>
+                <Rectangle x={startX} y={0} height={height} width={restWidth} fill="black" opacity={0.2} radius={[0,6,6,0]}/>
             </svg>
         )
     }
