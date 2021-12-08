8c2f6c91b1d15b33df986438b57a9dde39e6eb17
Valentin Kestelman <valaourok@gmail.com>
2021-12-08 17:49:34 +0100

    Fix cleanup warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import {Component} from "react";
 import Dashboard from "./layouts/Dashboard";
 import MainLayout from "./layouts/MainLayout";
+import {getUserActivityType, httpRequest} from "../index";
+import {Redirect} from "react-router-dom";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userId : parseInt(this.props.match.params.userId)
+            userId : parseInt(this.props.match.params.userId),
+            isLoading : true,
+            error : false
         }
     }
 
+    componentDidMount() {
+        httpRequest(this.setState.bind(this),getUserActivityType,this.state.userId)
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return (
+                <MainLayout>
+                    <div className="m-auto animate-pulse text-2xl text-bold">Données en chargement</div>
+                </MainLayout>
+            )
+        }
+
+        if (this.state.error) return <Redirect to="/erreur" />
+
         return (
             <MainLayout>
                 <Dashboard id={this.state.userId} goalAchieved={true}/>
--- src/components/ErrorDisplay.jsx
+++ src/components/ErrorDisplay.jsx
@@ -1,6 +1,5 @@
 import {Component} from "react";
 import PropTypes from "prop-types";
-import {Redirect} from "react-router-dom";
 
 /**
  * The component used to display an error when the API call fails
@@ -9,7 +8,7 @@ import {Redirect} from "react-router-dom";
 class ErrorDisplay extends Component {
     render() {
         return (
-            <Redirect to="/erreur" />
+            <div>Une erreur est survenue : {this.props.errorMsg}</div>
         )
     }
 }
